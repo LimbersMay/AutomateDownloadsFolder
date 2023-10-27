69dc9a99f0732ddc25a98afc8e2ce28e50e882e3
LimbersMay <limbermay1@gmail.com>
2023-10-27 15:09:29 -0600

    (entities ordered_file) atributo path agregado al las propiedades de la entidad

--- entities/ordered_file.py
+++ entities/ordered_file.py
@@ -2,6 +2,7 @@ import datetime
 
 
 class OrderedFile:
-    def __init__(self, name: str, ordered_date: datetime.date):
+    def __init__(self, name: str, ordered_date: datetime.date, path: str):
         self.name = name
         self.ordered_date = ordered_date
+        self.path = path
